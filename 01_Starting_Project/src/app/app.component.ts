8729a4331905f85fbd2b34c7719e5a11be42ccde
SnehalJagdale <80957573+SnehalJagdale31@users.noreply.github.com>
2024-11-14 17:32:03 +0530

    Update app.component.ts

--- 01_Starting_Project/src/app/app.component.ts
+++ 01_Starting_Project/src/app/app.component.ts
@@ -13,7 +13,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectUserId ='u1';
+  selectUserId ?: string ;
 
   get selectedUser(){
     return this.users.find((user) => user.id == this.selectUserId)!;
